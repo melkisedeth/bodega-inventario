3b78f65c1241555e1e63e6cc7e2c388fd1b44e32
Melky Tobias <melkytobias10@gmail.com>
2026-01-15 15:49:54 -0500

    ish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider, createTheme } from '@mui/material';
 import PrivateRoute from './components/auth/PrivateRoute';
-import Login from './pages/Login';
+import Login from './pages/login';
 // import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
